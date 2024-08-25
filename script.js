13067a61c6ecb512d4ddb4c88bfca92992905b95
Tejdeep Nookala <iamtejdeepn@gmail.com>
2024-08-26 01:13:50 +0530

    Can run in console properly with a bit of UI but only has one 1's

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 console.log("hello world")
 
+
+
 let board = [
     [0,0,0],
-    [0,1,1],
-    [1,0,0]
+    [0,0,0],
+    [0,0,0]
 ]
 
 let player1 = {
@@ -34,14 +36,59 @@ const initialiseGame = (function(){
         player2.score = 0
     }
 
+    const printBoard = ()=>{
+        // for(let row = 0; row<3; row++){
+        //     for(let column=0;column<3;column++){
+        //         console.log(board[row][column] )
+        //     }
+    
+        //     console.log(" || ")
+    
+        // }
+
+        console.table(board)
+    }
+
     return{
         resetGame,
+        printBoard
     }
 
 
 })()
 
 const duringGame = (function(){
+
+
+    //check if any win conditions hit
+    const checkWin = (icon)=>{
+
+        if(board[0][0]===board[1][1] && board[1][1]===board[2][2] && board[2][2] === icon){
+            return true;
+        }
+
+        if(board[0][2]===board[1][1] && board[1][1]===board[2][0] && board[1][1] === icon){
+            return true;
+        }
+
+        for(let row = 0; row<3; row++){
+            if(board[row][0] === board[row][1] && board[row][1] === board[row][2] && board[row][2] === icon ){
+                return true;
+            }
+        }
+
+
+        for(let column = 0; column<3; column++){
+            if(board[0][column] === board[1][column] && board[1][column] === board[2][column] && board[2][column] === icon ){
+                return true;
+            }
+        }
+
+        return false;
+
+
+
+    }
 
 
     //checks if there are any 0's in the board
@@ -57,37 +104,7 @@ const duringGame = (function(){
         return true;
     }
 
-    //check if any win conditions hit
-    const checkWin = ()=>{
-
-        let winCombos = 0;
-
-        if(board[0][0]===board[1][1] && board[1][1]===board[2][2] && board[2][2] === 1){
-            return true;
-        }
-
-        if(board[0][2]===board[1][1] && board[1][1]===board[2][0] && board[1][1] === 1){
-            return true;
-        }
-
-        for(let row = 0; row<3; row++){
-            if(board[row][0] === board[row][1] && board[row][1] === board[row][2] && board[row][2] === 1 ){
-                return true;
-            }
-        }
-
-
-        for(let column = 0; column<3; column++){
-            if(board[0][column] === board[1][column] && board[1][column] === board[2][column] && board[2][column] === 1 ){
-                return true;
-            }
-        }
-
-        return false;
-
-
-
-    }
+    
 
     return{
         checkWin,
@@ -98,15 +115,43 @@ const duringGame = (function(){
 })()
 
 
+const runGame = function(){
 
-// initialiseGame.resetGame();
-console.log(board[1][1])
+    initialiseGame.resetGame()
+    initialiseGame.printBoard()
 
-// if(duringGame.checkWin()){
-//     console.log("Win condition hit");
-// }
+    let moveRow
+    let moveCol
 
-console.log(duringGame.checkWin());
+    initialiseGame.printBoard()
+
+    console.log("Starting Game...");
+    
+
+    while(!duringGame.boardFull()){
+        console.log("Enter Move in Prompt");
+        
+
+        moveRow = prompt("Enter the Row (0-2) :", "0, 1 or 2");
+        moveCol = prompt("Enter the Column (0-2) :", "0, 1 or 2");
+
+        board[moveRow][moveCol] = 1
+
+        initialiseGame.printBoard()
+
+        if(duringGame.checkWin(1)){
+            console.log("Win Condition Hit!!!")
+            break;
+        }
+    }
+}
+
+
+
+runGame()
+
+
+
 
 
 
